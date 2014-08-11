591539b5c80c3fddf9c3635e058642ed74b68a24
Daria Jung <djj2115cu@gmail.com>
2014-08-11 15:07:51 -0400

    add config file to gitignore, pass ip and port, render using jinja

--- kitkat.py
+++ kitkat.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
+from CONFIG import IP_ADDRESS, CAT_PORT
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
-    return render_template('kitkat.html')
+def hello(ip=IP_ADDRESS, port=CAT_PORT):
+    print ip, port
+    return render_template('kitkat.html', ip=ip, port=port)
 
 if __name__ == "__main__":
     app.run()
